f246966acea4d1a4f5ad31d4b6b82cf1e746732a
Alejandro <alejandroguxman381@gmail.com>
2024-11-14 12:19:29 -0500

    Agregar archivos de proyecto.

---
+++ Salitre go/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Salitre_go
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
