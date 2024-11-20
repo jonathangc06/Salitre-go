2cb423a3fe202fcc4fb3cfceef20052e952890f9
PhoenixHear7 <cristian.vb01@hotmail.com>
2024-11-20 12:25:09 -0500

    Se agrego la pantalla de Itinerario y su respectiva logica

--- Salitre go/Itinerario.xaml.cs
+++ Salitre go/Itinerario.xaml.cs
@@ -1,9 +1,51 @@
-namespace Salitre_go;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 
-public partial class Itinerario : ContentPage
+namespace Salitre_go
 {
-	public Itinerario()
-	{
-		InitializeComponent();
-	}
+    public partial class Itinerario : ContentPage
+    {
+        public Itinerario()
+        {
+            InitializeComponent();
+            BindingContext = new ItinerarioViewModel();
+        }
+    }
+
+    public class ItinerarioViewModel
+    {
+        
+        public ObservableCollection<LugarHorario> Lugares { get; set; }
+
+        
+        public ICommand AgregarLugarCommand { get; }
+
+        public ItinerarioViewModel()
+        {
+            
+            Lugares = new ObservableCollection<LugarHorario>
+            {
+                new LugarHorario { Hora = "9:00 AM", Lugar = "Parque Acuático" },
+                new LugarHorario { Hora = "10:00 AM", Lugar = "Montaña Rusa Extrema" },
+                new LugarHorario { Hora = "11:00 AM", Lugar = "Casa del Terror" }
+            };
+
+            
+            AgregarLugarCommand = new Command(AgregarLugar);
+        }
+
+        /
+        private void AgregarLugar()
+        {
+            Lugares.Add(new LugarHorario { Hora = "", Lugar = "" });
+        }
+    }
+
+    
+    public class LugarHorario
+    {
+        public string Hora { get; set; }
+        public string Lugar { get; set; }
+    }
 }
