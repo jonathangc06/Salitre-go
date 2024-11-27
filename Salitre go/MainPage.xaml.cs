203acf7bbe6fa2fb470d6b9314aaea9db53f34bb
Sagarcia05 <sagarcia05@uan.edu.co>
2024-11-27 13:05:34 -0500

    Se agrego la interfaz recuperar contraseña

--- Salitre go/MainPage.xaml.cs
+++ Salitre go/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using Android.Content.Res;
+
 namespace Salitre_go
 {
     public partial class MainPage : ContentPage
@@ -12,6 +14,10 @@ namespace Salitre_go
         private async void OneButtonLogIn(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new menu());
+        }
+        private async void OneButtonForgot(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Recuperar_contraseña());
         }
     }
 
